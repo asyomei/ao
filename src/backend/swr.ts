b89766de5dccf0caa361559fd62c691a72cff217
vadim asyomei <vadim@asyomei.org>
2024-12-24 21:20:27 +0500

    refactor(swr): remove createdat, make code cleaner

--- src/backend/swr.ts
+++ src/backend/swr.ts
@@ -2,14 +2,6 @@ import { setTimeout as delay } from "timers/promises"
 
 export type Fetcher<T> = (props: { prev?: T }) => Promise<T>
 
-interface Resource<T> {
-  data: T
-  createdAt: number
-  updatedAt: number
-}
-
-const cache = new WeakMap<Function, Resource<any>>()
-
 interface Props<T> {
   ttlMs: number
   validateMs: number
@@ -21,56 +13,46 @@ export default function useSWR<T>({
   validateMs: validateTime,
   fetcher,
 }: Props<T>): () => Promise<T> {
-  let inserting = false
+  let data: T | undefined
+  let updatedAt = 0
+
   let updating = false
+  const finishUpdate = () => (updating = false)
 
-  const firstTime = () => !cache.has(fetcher)
-  const getResource = () => cache.get(fetcher)
-  const setResource = (value: Resource<T>) => cache.set(fetcher, value)
+  const firstTime = () => data === undefined
 
-  const fetchData = async () => {
-    const prev = getResource()?.data
-    return await fetcher({ prev })
+  const fetchData = () => fetcher({ prev: data })
+  const updateData = (newData: T) => {
+    data = newData
+    updatedAt = Date.now()
+  }
+
+  const needValidate = () => Date.now() - updatedAt > validateTime
+  const ttlExpired = () => Date.now() - updatedAt > ttl
+
+  const update = async ({ wait }: { wait: boolean }) => {
+    if (!updating) {
+      updating = true
+
+      const promise = fetchData().then(updateData).finally(finishUpdate)
+      if (wait) await promise
+
+      return data!
+    }
+
+    if (wait) while (updating) await delay(50)
+    return data!
   }
 
   return async () => {
-    const needValidate = () => Date.now() - getResource()!.createdAt > validateTime
-    const ttlExpired = () => Date.now() - getResource()!.updatedAt > ttl
-
     if (firstTime() || needValidate()) {
-      if (!inserting) {
-        inserting = true
-
-        try {
-          setResource({
-            data: await fetchData(),
-            createdAt: Date.now(),
-            updatedAt: Date.now(),
-          })
-        } finally {
-          inserting = false
-        }
-      }
-
-      while (inserting) await delay(50)
-      return getResource()!.data
+      return await update({ wait: true })
     }
 
-    if (ttlExpired() && !updating) {
-      updating = true
-
-      call(async () => {
-        Object.assign(getResource()!, {
-          data: await fetchData(),
-          updatedAt: Date.now(),
-        })
-      }).finally(() => {
-        updating = false
-      })
+    if (ttlExpired()) {
+      return await update({ wait: false })
     }
 
-    return getResource()!.data
+    return data!
   }
 }
-
-const call = <T>(fn: () => T) => fn()
