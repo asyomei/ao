1fa0f80407cc9c5ba0f7f85001cbc3f212ed0e3c
vadim asyomei <vadim@asyomei.org>
2024-12-24 23:22:57 +0500

    build: install deps on prod

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,14 @@ function sshp() {
 }
 
 echo "===> clean folder"
-sshp "rm -rf $REMOTE_PATH && mkdir $REMOTE_PATH"
+sshp "rm -rf $REMOTE_PATH/* && mkdir -p $REMOTE_PATH"
 
 echo "===> deploy"
+cp package.json dist/
 tar cf - -C dist . | sshp "tar x -C $REMOTE_PATH"
+
+echo "===> install deps"
+sshp "cd $REMOTE_PATH && pnpm i --prod"
 
 echo "===> restart server"
 curl --silent --basic --user "$ALWAYS_KEY account=$ALWAYS_ACCOUNT:" --data '' --request POST https://api.alwaysdata.com/v1/site/$ALWAYS_SITE_ID/restart/
