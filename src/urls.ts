d50355e69949009146dcf738e699a34499428301
vadim asyomei <vadim@asyomei.org>
2025-01-07 02:43:33 +0500

    refactor: group urls

---
+++ src/urls.ts
@@ -0,0 +1,16 @@
+export const me = {
+  vk: "https://vk.com/asyomei",
+  github: "https://github.com/asyomei",
+  lastfm: "https://last.fm/user/asyomei",
+  fourpda: "https://4pda.to/forum/index.php?showuser=8079654",
+  telegram: "https://t.me/asyomei",
+  mynickname: "https://mynickname.com/asyomei",
+}
+
+export const proj = {
+  website: `${me.github}/asyomei.org`,
+  himawari: `${me.github}/himawari-bot`,
+}
+
+export const himawariBot = "https://t.me/himawaridesu_bot"
+export const teidesu = "https://tei.su"
