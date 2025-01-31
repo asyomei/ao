7764a41ca436afe16867392c852d40f43eef0064
asyomei <vadim@asyomei.org>
2025-01-31 18:03:43 +0500

    feat(last-seen): add github service

---
+++ src/backend/last-seen/github.ts
@@ -0,0 +1,67 @@
+import { z } from 'zod'
+import { me } from '#/urls'
+import { HOUR } from '../consts'
+import { swr, ttlValidator } from '../swr'
+import type { LastSeenItem } from './types'
+
+const ResponseSchema = z.array(
+  z.object({
+    type: z.string(),
+    payload: z.any(),
+    repo: z.object({ name: z.string(), url: z.string() }),
+    created_at: z.string(),
+  }),
+)
+
+const API_URL = 'https://api.github.com/users/asyomei/events/public?per_page=1'
+
+async function fetchGithubEvent() {
+  const resp = await fetch(API_URL, {
+    headers: {
+      'user-agent': 'asyomei.org/1.0',
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  })
+  return ResponseSchema.parse(await resp.json())[0]
+}
+
+async function github(): Promise<LastSeenItem | undefined> {
+  const { type, payload, repo, created_at } = await fetchGithubEvent()
+
+  const getDescription: (() => string) | undefined = {
+    Create: () => `${payload.ref_type} created`,
+    Delete: () => `${payload.ref_type} deleted`,
+    Fork: () => 'forked',
+    Gollum: () => 'wiki updated',
+    IssueComment: () => `issue comment ${payload.action}`,
+    Issues: () => `issue ${payload.action}`,
+    Public: () => 'made public',
+    PullRequest: () => `pr ${payload.action}`,
+    Push: () => `pushed ${payload.distinct_size} commits`,
+    Release: () => `release ${payload.action}`,
+    Watch: () => 'starred',
+  }[type.replace('Event', '')]
+
+  if (!getDescription) return
+
+  return {
+    service: {
+      text: 'github',
+      url: me.github,
+    },
+    content: {
+      text: repo.name,
+      url: `https://github.com/${repo.name}`,
+    },
+    date: new Date(created_at),
+    suffix: `: ${getDescription()}`,
+  }
+}
+
+export default swr({
+  fetcher: github,
+  validate: ttlValidator({
+    update: 1 * HOUR,
+    refetch: 4 * HOUR,
+  }),
+})
--- src/backend/last-seen/index.ts
+++ src/backend/last-seen/index.ts
@@ -1,4 +1,5 @@
 import bsky from './bsky'
+import github from './github'
 import lastfm from './lastfm'
 import shiki from './shiki'
 import type { LastSeenItem } from './types'
@@ -9,7 +10,7 @@ export async function fetchLastSeenItems(): Promise<LastSeenItem[]> {
     if (item) items.push(item)
   }
 
-  const fetches = [lastfm(), bsky(), shiki()]
+  const fetches = [lastfm(), bsky(), shiki(), github()]
 
   await Promise.all(fetches.map(x => x.then(add, console.error)))
   return items.sort((a, b) => time(b) - time(a))
