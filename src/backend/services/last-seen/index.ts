a3416d2e19c4d846d06d30accc614b47a03f9afc
asyomei <vadim@asyomei.org>
2025-03-30 19:41:39 +0500

    feat(last-seen): add vndb service

--- src/backend/services/last-seen/index.ts
+++ src/backend/services/last-seen/index.ts
@@ -3,6 +3,7 @@ import github from './github'
 import lastfm from './lastfm'
 import shiki from './shiki'
 import type { LastSeenItem } from './types'
+import vndb from './vndb'
 
 export async function fetchLastSeenItems(): Promise<LastSeenItem[]> {
   const items: LastSeenItem[] = []
@@ -10,7 +11,7 @@ export async function fetchLastSeenItems(): Promise<LastSeenItem[]> {
     if (item) items.push(item)
   }
 
-  const fetches = [lastfm(), bsky(), shiki(), github()]
+  const fetches = [lastfm(), bsky(), shiki(), github(), vndb()]
 
   await Promise.all(fetches.map(x => x.then(add, console.error)))
   return items.sort((a, b) => time(b) - time(a))
